1edb0b396bafc235f2c51eeeaa0f560500cb06c2
harshal-dupare <harshal333hd@gmail.com>
2022-12-05 23:53:07 +0530

    merging all union find using template booleans

--- lib/data_structure/union_find.hpp
+++ lib/data_structure/union_find.hpp
@@ -2,102 +2,30 @@
 #include <vector>
 #include <queue>
 #include <iostream>
-// FIXME check for which one is more optimal in x=f[x]=f[f[x]] or using queue with size heuristic and without
-// FIXME combine into 1 big template guy to accomodate all of them
 
-/**
- * ( NO Size heuristic, Level-2 path compression)
- */
-template <typename I>
-class union_find_2pc
-{
-public:
-    std::vector<I> parent;
-    I n;
-    union_find_szh_2pc(I _n)
-    {
-        this->n = _n;
-        this->parent = std::vector<I>(_n, -1); // no element in the sets hence all empty as -1
-    }
-    void make_set(I x)
-    {
-        this->parent[x] = x;
-    }
-    bool union_sets(I x, I y)
-    {
-        I sx = this->find_set(x);
-        I sy = this->find_set(y);
-        if (sx != sy)
-        {
-            this->parent[sy] = sx;
-            return true;
-        }
-        return false;
-    }
-    I find_set(I x)
-    {
-        if (this->parent[x] == -1)
-            return -1;
-        // decreases the path length to top by half for x and everyone above it
-        while (x != this->parent[x])
-        {
-            x = this->parent[x] = this->parent[this->parent[x]]; 
-        }
-        return x;
-    }
-    I _const_find_set(I x) const
-    {
-        if (this->parent[x] == -1)
-            return -1;
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-        }
-        return x;
-    }
-    I _count_jumps_to_find_parent_of(I x) const
-    {
-        I ct = 0;
-        if (this->parent[x] == -1)
-            return ct;
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-            ct++;
-        }
-        return ct;
-    }
-    friend std::ostream &operator<<(std::ostream &os, const union_find_szh_2pc<I> &uf)
-    {
-        for (I i = 0; i < uf.n; i++)
-        {
-            os << i << ":" << uf._const_find_set(i) << ", ";
-        }
-        os << "\n";
-        return os;
-    }
-};
-
-/**
- * ( Size heuristic, Level-2 path compression)
- */
-template <typename I>
-class union_find_szh_2pc
+template <typename I, const bool Size_Heuristic = false, const bool Compression_Order_2 = true, const bool Inf_Path_Compression_Queue = false>
+class union_find
 {
 public:
     std::vector<I> parent;
     std::vector<I> size;
     I n;
-    union_find_szh_2pc(I _n)
+    union_find(I _n)
     {
         this->n = _n;
         this->parent = std::vector<I>(_n, -1); // no element in the sets hence all empty as -1
-        this->size = std::vector<I>(_n, 0);    // no element in the sets hence all empty as -
+        if (Size_Heuristic)
+        {
+            this->size = std::vector<I>(_n, 0); // no element in the sets hence all empty as
+        }
     }
     void make_set(I x)
     {
         this->parent[x] = x;
-        this->size[x] = 1;
+        if (Size_Heuristic)
+        {
+            this->size[x] = 1;
+        }
     }
     bool union_sets(I x, I y)
     {
@@ -105,23 +33,55 @@ public:
         I sy = this->find_set(y);
         if (sx != sy)
         {
-            // sx must have the larger size
-            if (this->size[sx] < this->size[sy])
-                std::swap(sx, sy);
+            if (Size_Heuristic)
+            {
+                // sx must have the larger size
+                if (this->size[sx] < this->size[sy])
+                {
+                    std::swap(sx, sy);
+                }
+                this->size[sx] += this->size[sy];
+            }
             this->parent[sy] = sx;
-            this->size[sx] += this->size[sy];
             return true;
         }
         return false;
     }
+    // FIXME: convert this to enable if
     I find_set(I x)
     {
         if (this->parent[x] == -1)
             return -1;
         // decreases the path length to top by half for x and everyone above it
+        if (Inf_Path_Compression_Queue)
+        {
+            std::queue<I> q;
+            q.push(x);
+        }
         while (x != this->parent[x])
         {
-            x = this->parent[x] = this->parent[this->parent[x]]; 
+            if (Compression_Order_2)
+            {
+                x = this->parent[x] = this->parent[this->parent[x]];
+            }
+            else
+            {
+                x = this->parent[x];
+            }
+
+            if (Inf_Path_Compression_Queue)
+            {
+                q.push(x);
+            }
+        }
+
+        if (Inf_Path_Compression_Queue)
+        {
+            while (!q.empty())
+            {
+                this->parent[q.front()] = x;
+                q.pop();
+            }
         }
         return x;
     }
@@ -147,19 +107,27 @@ public:
         }
         return ct;
     }
-    friend std::ostream &operator<<(std::ostream &os, const union_find_szh_2pc<I> &uf)
+    friend std::ostream &operator<<(std::ostream &os, const union_find<I, Size_Heuristic, Compression_Order_2, Inf_Path_Compression_Queue> &uf)
     {
         for (I i = 0; i < uf.n; i++)
         {
             I u = uf._const_find_set(i);
-            os << i << ":(" << u << ",";
-            if (u == -1)
+
+            if (Size_Heuristic)
             {
-                os << "0), ";
+                os << i << ":(" << u << ",";
+                if (u == -1)
+                {
+                    os << "0), ";
+                }
+                else
+                {
+                    os << uf.size[u] << "), ";
+                }
             }
             else
             {
-                os << uf.size[u] << "), ";
+                os << i << ":" << uf._const_find_set(i) << ", ";
             }
         }
         os << "\n";
@@ -167,178 +135,3 @@ public:
     }
 };
 
-/**
- * ( NO Size heuristic, Level-inf path compression using queue )
- */
-template <typename I>
-class union_find_infpcq
-{
-public:
-    std::vector<I> parent;
-    I n;
-    union_find_infpcq(I _n)
-    {
-        this->n = _n;
-        this->parent = std::vector<I>(_n, -1); // no element in the sets hence all empty as -1
-    }
-    void make_set(I x)
-    {
-        this->parent[x] = x;
-    }
-    bool union_sets(I x, I y)
-    {
-        I sx = this->find_set(x);
-        I sy = this->find_set(y);
-        if (sx != sy)
-        {
-            this->parent[sy] = sx;
-            return true;
-        }
-        return false;
-    }
-    I find_set(I x)
-    {
-        if (this->parent[x] == -1)
-            return -1;
-        std::queue<I> q;
-        q.push(x);
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-            q.push(x);
-        }
-        while (!q.empty())
-        {
-            this->parent[q.front()] = x;
-            q.pop();
-        }
-        return x;
-    }
-    I _const_find_set(I x) const
-    {
-        if (this->parent[x] == -1)
-            return -1;
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-        }
-        return x;
-    }
-    I _count_jumps_to_find_parent_of(I x) const
-    {
-        I ct = 0;
-        if (this->parent[x] == -1)
-            return ct;
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-            ct++;
-        }
-        return ct;
-    }
-    friend std::ostream &operator<<(std::ostream &os, const union_find_infpcq<I> &uf)
-    {
-        for (I i = 0; i < uf.n; i++)
-        {
-            os << i << ":" << uf._const_find_set(i) << ", ";
-        }
-        os << "\n";
-        return os;
-    }
-};
-
-/**
- * ( Size heuristic, Level-inf path compression using queue )
- */
-template <typename I>
-class union_find_szh_infpcq
-{
-public:
-    std::vector<I> parent;
-    std::vector<I> size;
-    I n;
-    union_find_szh_infpcq(I _n)
-    {
-        this->n = _n;
-        this->parent = std::vector<I>(_n, -1); // no element in the sets hence all empty as -1
-        this->size = std::vector<I>(_n, 0);    // no element in the sets hence all empty as -
-    }
-    void make_set(I x)
-    {
-        this->parent[x] = x;
-        this->size[x] = 1;
-    }
-    bool union_sets(I x, I y)
-    {
-        I sx = this->find_set(x);
-        I sy = this->find_set(y);
-        if (sx != sy)
-        {
-            // sx must have the larger size
-            if (this->size[sx] < this->size[sy])
-                std::swap(sx, sy);
-            this->parent[sy] = sx;
-            this->size[sx] += this->size[sy];
-            return true;
-        }
-        return false;
-    }
-    I find_set(I x)
-    {
-        if (this->parent[x] == -1)
-            return -1;
-        std::queue<I> q;
-        q.push(x);
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-            q.push(x);
-        }
-        while (!q.empty())
-        {
-            this->parent[q.front()] = x;
-            q.pop();
-        }
-        return x;
-    }
-    I _const_find_set(I x) const
-    {
-        if (this->parent[x] == -1)
-            return -1;
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-        }
-        return x;
-    }
-    I _count_jumps_to_find_parent_of(I x) const
-    {
-        I ct = 0;
-        if (this->parent[x] == -1)
-            return ct;
-        while (x != this->parent[x])
-        {
-            x = this->parent[x];
-            ct++;
-        }
-        return ct;
-    }
-    friend std::ostream &operator<<(std::ostream &os, const union_find_szh_infpcq<I> &uf)
-    {
-        for (I i = 0; i < uf.n; i++)
-        {
-            I u = uf._const_find_set(i);
-            os << i << ":(" << u << ",";
-            if (u == -1)
-            {
-                os << "0), ";
-            }
-            else
-            {
-                os << uf.size[u] << "), ";
-            }
-        }
-        os << "\n";
-        return os;
-    }
-};
